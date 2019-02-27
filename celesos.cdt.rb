6b4281005bb99773240a8d9227d84647d13a0d93
celes-dev <hubery.zhang@blockc.pro>
2019-02-27 16:05:57 +0800

    modify rb file

--- celesos.cdt.rb
+++ celesos.cdt.rb
@@ -2,7 +2,7 @@ class CelesosCdt < Formula
 
    homepage "https://github.com/eosio/celesos.cdt"
    revision 0
-   url "https://github.com/celes-dev/celesos.cdt/archive/celesos.cdt-0.9.0.mojave.bottle.tar.gz.tar.gz"
+   url "https://github.com/celes-dev/celesos.cdt/archive/v0.9.0/celesos.cdt-0.9.0.mojave.bottle.tar.gz"
    version "0.9.0"
    
    option :universal
@@ -22,7 +22,7 @@ class CelesosCdt < Formula
   
    bottle do
       root_url "https://github.com/celes-dev/celesos.cdt/releases/download/v0.9.0/celesos.cdt-0.9.0.mojave.bottle.tar.gz"
-      sha256 "6dfca6c4cbbdf91d57bd6d0146a2dc937ff79593507b6b10a8e1bcb008f0fa43" => :mojave
+      sha256 "79e607211197f3003340353b2d462b7af808c49b02adef268fc536c948153874" => :mojave
    end
    def install
       raise "Error, only supporting binary packages at this time"
